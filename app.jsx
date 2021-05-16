0770791bfb2a3feece898680018cc0acb2ed59c4
simone <sorzesimone1@gmail.com>
2021-05-16 17:23:15 +0200

    testing formatting

--- app.jsx
+++ app.jsx
@@ -1,18 +1,25 @@
-import "./styles.css";
+import './styles.css'
+import AFunction from './button'
 
 const app = () => {
-    (x) => x;
-    console.log(
-        "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
-    );
-    console.log("who");
-    console.log("who");
-    console.log("who");
-    console.log("who");
-    console.log("who");
-    console.log("who");
-};
+    console.log('something')
 
-const test = () => {
-    (x) => x;
-};
+    console.log('who')
+    console.log('who')
+    console.log('who')
+    console.log('who')
+    console.log('who')
+
+    console.log('who')
+}
+
+export default function Meer() {
+    console.log('hey')
+    return 5
+}
+
+class MyClass {
+    constructor(test) {
+        this.test = test
+    }
+}
